4cce83e1fb51befcf6b6671b804e90e9bfaf8d89
FlechaMaker <flechacreador@gmail.com>
2024-08-15 14:53:19 +0900

    簡易的な認証機能を実装

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,19 +2,33 @@ import "server-cli-only";
 
 import React from "react";
 import { redirect } from "next/navigation";
-
-// import { getAuthenticatedAppForUser } from "@/lib/firebase/serverApp";
+import { cookies } from "next/headers";
+import { getCookie } from "cookies-next";
+import { User } from "firebase/auth";
 
 export default async function ProtectedPageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const { currentUser } = await getAuthenticatedAppForUser();
+  const currentUserJSON = getCookie("user", { cookies });
 
-  // if (!currentUser) {
-  //   redirect("/");
-  // }
+  let uid = null;
+
+  if (currentUserJSON) {
+    try {
+      const currentUser = JSON.parse(currentUserJSON) as User;
+
+      uid = currentUser.uid;
+      console.log(currentUser);
+    } catch (error) {
+      uid = null;
+    }
+  }
+
+  if (!uid) {
+    redirect("/login");
+  }
 
   return <>{children}</>;
 }
