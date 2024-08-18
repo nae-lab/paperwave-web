19b8d6e932a8c60a743c1b68260220908a35800c
FlechaMaker <flechacreador@gmail.com>
2024-08-18 15:57:28 +0900

    "force-dynamic" を消すとfirebaseのエラーが出ることがわかった．

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ import Navbar from "@/components/navbar";
 
 // Force next.js to treat this route as server-side rendered
 // Without this line, during the build process, next.js will treat this route as static and build a static HTML file for it
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
   title: {
