02f54c4d35cb422e0b99db3057306f121ed95e93
FlechaMaker <flechacreador@gmail.com>
2024-08-20 14:02:43 +0900

    未完成の番組一覧ページのデバッグ用コード部分を削除

--- app/(protected)/channels/[channelId]/page.tsx
+++ app/(protected)/channels/[channelId]/page.tsx
@@ -6,17 +6,9 @@ import React from "react";
 import { Button } from "@nextui-org/react";
 
 import Player from "@/components/player";
-import { getEpisode, setSeedData } from "@/lib/episodes";
+import { setSeedData } from "@/lib/episodes";
 
 const ProgramsPage = () => {
-  const [docs, setDocs] = React.useState("");
-
-  const handleGetDocs = async () => {
-    const data = await getEpisode();
-
-    setDocs(JSON.stringify(data));
-  };
-
   return (
     <div className="flex h-full w-full flex-col flex-nowrap items-stretch justify-start gap-3.5">
       <div className="flex justify-start">
@@ -25,8 +17,6 @@ const ProgramsPage = () => {
         </h1>
       </div>
       <Button onPress={() => setSeedData()}>Set Seed Data</Button>
-      <Button onPress={handleGetDocs}>Get Docs</Button>
-      <p>{docs}</p>
       <div className="flex flex-col items-center justify-start gap-5">
         <Player programId="hoge" />
         <Player programId="hoge" />
